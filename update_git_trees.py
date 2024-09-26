664e30639133695e47578da93a4d6807263a3e27
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-09-26 12:39:37 -0700

    update workflow scripts

--- update_git_trees.py
+++ update_git_trees.py
@@ -1,4 +1,4 @@
-from json import dump, load
+from json import dumps, load
 from subprocess import run
 
 # Update spdlog git-tree
@@ -6,10 +6,13 @@ spdlog_git_tree = run(
     ["git", "rev-parse", "HEAD:ports/spdlog"], capture_output=True
 ).stdout.decode()
 
-with open("./versions/s-/spdlog.json", "r") as f:
+with open("./versions/s-/spdlog.json") as f:
     version = load(f)
 
 version["versions"][0]["git-tree"] = spdlog_git_tree.strip("\n")
 
-with open("./versions/s-/spdlog.json", "w") as f:
-    dump(version, f, indent=2)
+version_str = dumps(version, indent=2)
+version_str += "\r\n"
+
+with open("./versions/s-/spdlog.json", "w", newline="\r\n") as f:
+    f.write(version_str)
--- update_vcpkg_ports.py
+++ update_vcpkg_ports.py
@@ -1,5 +1,5 @@
 from hashlib import sha512
-from json import dump, load
+from json import dumps, load
 from re import sub
 from subprocess import run
 from urllib.request import urlretrieve
@@ -19,31 +19,40 @@ spdlog_ref, spdlog_tag = (
 spdlog_tag = spdlog_tag.replace("refs/tags/v", "")
 
 # Update baseline.json
-with open("./versions/baseline.json", "r") as f:
+with open("./versions/baseline.json") as f:
     baseline = load(f)
 
 baseline["default"]["spdlog"]["baseline"] = spdlog_tag
 
-with open("./versions/baseline.json", "w") as f:
-    dump(baseline, f, indent=2)
+baseline_str = dumps(baseline, indent=2)
+baseline_str += "\r\n"
+
+with open("./versions/baseline.json", "w", newline="\r\n") as f:
+    f.write(baseline_str)
 
 # Update spdlog/vcpkg.json
-with open("./ports/spdlog/vcpkg.json", "r") as f:
+with open("./ports/spdlog/vcpkg.json") as f:
     vcpkg_json = load(f)
 
 vcpkg_json["version-semver"] = spdlog_tag
 
-with open("./ports/spdlog/vcpkg.json", "w") as f:
-    dump(vcpkg_json, f, indent=2)
+vcpkg_json_str = dumps(vcpkg_json, indent=2)
+vcpkg_json_str += "\r\n"
+
+with open("./ports/spdlog/vcpkg.json", "w", newline="\r\n") as f:
+    f.write(vcpkg_json_str)
 
 # Update versions/s-/spdlog.json
-with open("./versions/s-/spdlog.json", "r") as f:
+with open("./versions/s-/spdlog.json") as f:
     spdlog_json = load(f)
 
 spdlog_json["versions"][0]["version-semver"] = spdlog_tag
 
-with open("./versions/s-/spdlog.json", "w") as f:
-    dump(spdlog_json, f, indent=2)
+spdlog_json_str = dumps(spdlog_json, indent=2)
+spdlog_json_str += "\r\n"
+
+with open("./versions/s-/spdlog.json", "w", newline="\r\n") as f:
+    f.write(spdlog_json_str)
 
 # Update spdlog portfile
 spdlog_archive, _ = urlretrieve(f"{spdlog_repo}/archive/refs/tags/v{spdlog_tag}.tar.gz")
@@ -51,10 +60,11 @@ spdlog_archive, _ = urlretrieve(f"{spdlog_repo}/archive/refs/tags/v{spdlog_tag}.
 with open(spdlog_archive, "rb") as f:
     spdlog_sha = sha512(f.read()).hexdigest()
 
-with open("./ports/spdlog/portfile.cmake", "r") as f:
+with open("./ports/spdlog/portfile.cmake") as f:
     portfile = f.readlines()
-    portfile[3] = sub(r"(REF).*", f"REF v{spdlog_tag}", portfile[3])
-    portfile[4] = sub(r"(SHA512).*", f"SHA512 {spdlog_sha}", portfile[4])
 
-with open("./ports/spdlog/portfile.cmake", "w") as f:
+portfile[3] = sub(r"(REF).*", f"REF v{spdlog_tag}", portfile[3])
+portfile[4] = sub(r"(SHA512).*", f"SHA512 {spdlog_sha}", portfile[4])
+
+with open("./ports/spdlog/portfile.cmake", "w", newline="\r\n") as f:
     f.writelines(portfile)
